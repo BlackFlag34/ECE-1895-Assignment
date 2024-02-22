b81af1bb7e2d069b795f1d631e2e2daf36a482af
justusdifference <118774847+justusdifference@users.noreply.github.com>
2024-02-22 15:05:09 -0500

    Add files via upload

---
+++ Blink_3.ino
@@ -0,0 +1,25 @@
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(6, OUTPUT);    // sets the digital pin as output
+  pinMode(7, OUTPUT);    // sets the digital pin as output
+  pinMode(8, OUTPUT);    // sets the digital pin as output  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  digitalWrite(6, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(6, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+  digitalWrite(7, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(7, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+  digitalWrite(8, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(8, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
